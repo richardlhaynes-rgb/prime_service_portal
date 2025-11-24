bc07e9655b7d7ee9c1eb2f97d9c4acfe2de542e4
Richard Haynes <richard.haynes@primeeng.com>
2025-11-23 22:56:15 -0500

    Visual: Add Official Logo, Header Title, and Favicon

--- config/settings.py
+++ config/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static files configuration
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
